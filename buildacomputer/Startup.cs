ba85e55a9daa043366b47852d32e4378936561e3
Robert G <rtd91@live.com>
2015-08-20 10:49:44 -0400

    Initial Build Blank MVC Template
    Includes Individual User Accounts

---
+++ buildacomputer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(buildacomputer.Startup))]
+namespace buildacomputer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
