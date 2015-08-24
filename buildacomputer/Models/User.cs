a23a00f8706c6f6095ff54a8ddb3ab86cc105ffb
mjwweber <marajwweber@gmail.com>
2015-08-24 11:21:55 -0400

    UserModelEdited
    altered user model

--- buildacomputer/Models/User.cs
+++ buildacomputer/Models/User.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace buildacomputer.Models
+namespace buildacomputer.Models //creates the model used for users
 {
     public class User
     {
