fad73087220706b21259b5d5ffeab2dd06f87d65
boviguerre <jonathan.guerrette@gmail.com>
2015-08-24 16:39:58 -0400

    Added the BuildsController scaffolding

---
+++ buildacomputer/Controllers/BuildsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using buildacomputer.Models;
+
+namespace buildacomputer.Controllers
+{
+    public class BuildsController : Controller
+    {
+        private PartsAndUsersContext db = new PartsAndUsersContext();
+
+        // GET: Builds
+        public ActionResult Index()
+        {
+            var build = db.Build.Include(b => b.computer_cases).Include(b => b.hard_drives).Include(b => b.memories).Include(b => b.motherboards).Include(b => b.optical_drives).Include(b => b.power_supplies).Include(b => b.sound_cards).Include(b => b.video_adapters);
+            return View(build.ToList());
+        }
+
+        // GET: Builds/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Build build = db.Build.Find(id);
+            if (build == null)
+            {
+                return HttpNotFound();
+            }
+            return View(build);
+        }
+
+        // GET: Builds/Create
+        public ActionResult Create()
+        {
+            ViewBag.computer_case_id = new SelectList(db.computer_cases, "computer_case_id", "color");
+            ViewBag.hard_drive_id = new SelectList(db.hard_drives, "hard_drive_id", "hard_drive_name");
+            ViewBag.memory_id = new SelectList(db.memories, "memory_id", "memory_name");
+            ViewBag.motherboard_id = new SelectList(db.motherboards, "motherboard_id", "motherboard_name");
+            ViewBag.optical_drive_id = new SelectList(db.optical_drives, "optical_drive_id", "optical_drive_name");
+            ViewBag.power_supply_id = new SelectList(db.power_supplies, "power_supply_id", "power_supply_name");
+            ViewBag.sound_card_id = new SelectList(db.sound_cards, "sound_card_id", "sound_card_name");
+            ViewBag.video_adapter_id = new SelectList(db.video_adapters, "video_adapter_id", "video_adapter_name");
+            return View();
+        }
+
+        // POST: Builds/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "buildID,motherboard_id,computer_case_id,hard_drive_id,optical_drive_id,power_supply_id,processer_id,sound_card_id,video_adapter_id,memory_id,buildType,iterator,BuildTime")] Build build)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Build.Add(build);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.computer_case_id = new SelectList(db.computer_cases, "computer_case_id", "color", build.computer_case_id);
+            ViewBag.hard_drive_id = new SelectList(db.hard_drives, "hard_drive_id", "hard_drive_name", build.hard_drive_id);
+            ViewBag.memory_id = new SelectList(db.memories, "memory_id", "memory_name", build.memory_id);
+            ViewBag.motherboard_id = new SelectList(db.motherboards, "motherboard_id", "motherboard_name", build.motherboard_id);
+            ViewBag.optical_drive_id = new SelectList(db.optical_drives, "optical_drive_id", "optical_drive_name", build.optical_drive_id);
+            ViewBag.power_supply_id = new SelectList(db.power_supplies, "power_supply_id", "power_supply_name", build.power_supply_id);
+            ViewBag.sound_card_id = new SelectList(db.sound_cards, "sound_card_id", "sound_card_name", build.sound_card_id);
+            ViewBag.video_adapter_id = new SelectList(db.video_adapters, "video_adapter_id", "video_adapter_name", build.video_adapter_id);
+            return View(build);
+        }
+
+        // GET: Builds/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Build build = db.Build.Find(id);
+            if (build == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.computer_case_id = new SelectList(db.computer_cases, "computer_case_id", "color", build.computer_case_id);
+            ViewBag.hard_drive_id = new SelectList(db.hard_drives, "hard_drive_id", "hard_drive_name", build.hard_drive_id);
+            ViewBag.memory_id = new SelectList(db.memories, "memory_id", "memory_name", build.memory_id);
+            ViewBag.motherboard_id = new SelectList(db.motherboards, "motherboard_id", "motherboard_name", build.motherboard_id);
+            ViewBag.optical_drive_id = new SelectList(db.optical_drives, "optical_drive_id", "optical_drive_name", build.optical_drive_id);
+            ViewBag.power_supply_id = new SelectList(db.power_supplies, "power_supply_id", "power_supply_name", build.power_supply_id);
+            ViewBag.sound_card_id = new SelectList(db.sound_cards, "sound_card_id", "sound_card_name", build.sound_card_id);
+            ViewBag.video_adapter_id = new SelectList(db.video_adapters, "video_adapter_id", "video_adapter_name", build.video_adapter_id);
+            return View(build);
+        }
+
+        // POST: Builds/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "buildID,motherboard_id,computer_case_id,hard_drive_id,optical_drive_id,power_supply_id,processer_id,sound_card_id,video_adapter_id,memory_id,buildType,iterator,BuildTime")] Build build)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(build).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.computer_case_id = new SelectList(db.computer_cases, "computer_case_id", "color", build.computer_case_id);
+            ViewBag.hard_drive_id = new SelectList(db.hard_drives, "hard_drive_id", "hard_drive_name", build.hard_drive_id);
+            ViewBag.memory_id = new SelectList(db.memories, "memory_id", "memory_name", build.memory_id);
+            ViewBag.motherboard_id = new SelectList(db.motherboards, "motherboard_id", "motherboard_name", build.motherboard_id);
+            ViewBag.optical_drive_id = new SelectList(db.optical_drives, "optical_drive_id", "optical_drive_name", build.optical_drive_id);
+            ViewBag.power_supply_id = new SelectList(db.power_supplies, "power_supply_id", "power_supply_name", build.power_supply_id);
+            ViewBag.sound_card_id = new SelectList(db.sound_cards, "sound_card_id", "sound_card_name", build.sound_card_id);
+            ViewBag.video_adapter_id = new SelectList(db.video_adapters, "video_adapter_id", "video_adapter_name", build.video_adapter_id);
+            return View(build);
+        }
+
+        // GET: Builds/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Build build = db.Build.Find(id);
+            if (build == null)
+            {
+                return HttpNotFound();
+            }
+            return View(build);
+        }
+
+        // POST: Builds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Build build = db.Build.Find(id);
+            db.Build.Remove(build);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
