d199ebd6fd0fba0de8052621b7338e4868fe93ae
mjwweber <marajwweber@gmail.com>
2015-08-24 11:17:28 -0400

    UserModelEdited
    User model edited to incorporate foreign keys

--- buildacomputer/Models/User.cs
+++ buildacomputer/Models/User.cs
@@ -9,11 +9,14 @@ namespace buildacomputer.Models
 {
     public class User
     {
-        public string userID {get; set;}
+        public string Id {get; set;}
         public string firstName {get; set;}
         public string lastName {get; set;}
         public string question {get; set;}
         public string answer {get;set;}
         public string title { get; set; }
+
+        public virtual AspNetUsers AspNetUsers { get; set; }
+        public virtual ICollection<UserBuilds> UserBuilds { get; set; }
     }
 }
