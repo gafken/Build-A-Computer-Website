7290437b2f9e3a126e94abc8fa73f4e77bcf65cd
mjwweber <marajwweber@gmail.com>
2015-08-24 10:14:31 -0400

    UserModel
    Created and filled the user model

---
+++ buildacomputer/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace buildacomputer.Models
+{
+    public class User
+    {
+        public string userID {get; set;}
+        public string firstName {get; set;}
+        public string lastName {get; set;}
+        public string question {get; set;}
+        public string answer {get;set;}
+        public string title { get; set; }
+    }
+}
